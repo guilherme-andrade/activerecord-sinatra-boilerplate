dc5e8735e79f3115889036e7048f931ac283d52d
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2020-01-27 17:42:07 +0100

    finito

--- app.rb
+++ app.rb
@@ -1,8 +1,29 @@
 require_relative "config/application"
+
 require "sinatra"
 require "sinatra/reloader"
 require "sinatra/activerecord"
+require 'pry-byebug'
 
-get "/" do
-  "Hello world!"
+require_relative 'models/restaurant'
+
+get "/restaurants" do
+	@restaurants = Restaurant.all
+	erb :index
+end
+
+post "/restaurants" do
+	@restaurant = Restaurant.create(name: params["name"], city: params["city"])
+	erb :show
+end
+
+# this matches /restaurants/new only
+get "/restaurants/new" do
+	erb :new
+end
+
+# this matches /restaurants/any_string
+get "/restaurants/:id" do
+	@restaurant = Restaurant.find(params["id"])
+	erb :show
 end
